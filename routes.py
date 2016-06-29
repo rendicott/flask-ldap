f50779aa8552bcf25a49b7347e7a6562cf250382
Russell ENdicott <rendicott@cardlytics.com>
2016-06-29 16:41:09 -0400

    restructuring

--- routes.py
+++ routes.py
@@ -25,7 +25,8 @@ from flask_login import login_user
 #from wtforms import PasswordField
 from flask_ldap_login import LDAPLoginForm, LDAPLoginManager
 
-from ldap_config import LDAP
+from ldap_config import LDAP_SETTINGS
+
 
 app = Flask(__name__)      
 app.secret_key = 'dank warhammer'
@@ -33,47 +34,74 @@ login_manager = LoginManager(app)
 
 users = {}
 
-app.config.update(LDAP=LDAP)
-ldap_mgr = LDAPLoginManager(app)
+for key,val in LDAP_SETTINGS.iteritems():
+    app.config[key] = val
 
-@ldap_mgr.save_user
-def save_user(username, userdata):
-    users[username] = User(username, userdata)
-    return users[username]
+login_manager = LoginManager(app)              # Setup a Flask-Login Manager
+ldap_manager = LDAP3LoginManager(app)          # Setup a LDAP3 Login Manager.
 
+# Declare an Object Model for the user, and make it comply with the
+# flask-login UserMixin mixin.
 class User(UserMixin):
-    def __init__(self):
-        pass
+    def __init__(self, dn, username, data):
+        self.dn = dn
+        self.username = username
+        self.data = data
 
-#class LoginForm(Form):
-#    username = TextField('Username')
-#    password = PasswordField('Password')
-#    def validate_on_submit(self):
-#        return True
+    def __repr__(self):
+        return self.dn
+
+    def get_id(self):
+        return self.dn
+
+# Declare a User Loader for Flask-Login.
+# Simply returns the User if it exists in our 'database', otherwise
+# returns None.
+@login_manager.user_loader
+def load_user(id):
+    if id in users:
+        return users[id]
+    return None
+
+# Declare The User Saver for Flask-Ldap3-Login
+# This method is called whenever a LDAPLoginForm() successfully validates.
+# Here you have to save the user, and return it so it can be used in the
+# login controller.
+@ldap_manager.save_user
+def save_user(dn, username, data, memberships):
+    user = User(dn, username, data)
+    users[dn] = user
+    return user
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+    # Redirect users who are not logged in.
+    if not current_user or current_user.is_anonymous:
+        return redirect(url_for('login'))
+
+    return render_template_string('home.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
- 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+    # Redirect users who are not logged in.
+    if not current_user or current_user.is_anonymous:
+        return redirect(url_for('login'))
+    
+    return render_template('about.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def ldap_login():
+@app.route('/login', methods=['GET','POST'])
+def login():
+    # Instantiate a LDAPLoginForm which has a validator to check if the user
+    # exists in LDAP.
+    form = LDAPLoginForm()
 
-    form = LDAPLoginForm(request.form)
     if form.validate_on_submit():
-        login_user(form.user, remember=True)
-        print "Valid"
-        return redirect('/')
-    else:
-        print "Invalid"
-    return render_template('login.html', form=form)
+        # Successfully logged in, We can now access the saved user object
+        # via form.user.
+        login_user(form.user) # Tell flask-login to log them in.
+        return redirect('/')  # Send them home
+
+    return render_template_string('login.html', form=form)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5001)
